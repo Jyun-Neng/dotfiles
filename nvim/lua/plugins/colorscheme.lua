b4330eea1177a9a955a4683e316e54902254a908
Jyun-Neng <jyunnengji@gmail.com>
2025-12-16 00:34:11 +0800

    NVIM: Enable transparent background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin" },
+  { "catppuccin/nvim", name = "catppuccin", opts = {
+    transparent_background = true,
+  } },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
